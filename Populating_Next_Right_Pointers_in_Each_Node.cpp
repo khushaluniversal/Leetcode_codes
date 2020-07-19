8d0558fb692cc543e0db09aaeb7c171665ef261b
KHUSHAL GUMGAONKAR <khushaluniversal@users.noreply.github.com>
2020-07-19 19:25:16 +0530

    Create Populating_Next_Right_Pointers_in_Each_Node.cpp

---
+++ Populating_Next_Right_Pointers_in_Each_Node.cpp
@@ -0,0 +1,78 @@
+/*
+You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+ 
+
+Follow up:
+
+You may only use constant extra space.
+Recursive approach is fine, you may assume implicit stack space does not count as extra space for this problem.
+ 
+
+Example 1:
+
+
+
+Input: root = [1,2,3,4,5,6,7]
+Output: [1,#,2,3,#,4,5,6,7,#]
+Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+ 
+
+Constraints:
+
+The number of nodes in the given tree is less than 4096.
+-1000 <= node.val <= 1000
+*/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    //here we are setting next pointer of parent must be set before it's children
+    //using preoreder traversal we are doing same
+    void connect_util(Node* root)
+    {
+        if(root == NULL)
+            return;
+        
+        if(root->left)
+            root->left->next = root->right;
+        
+        if(root->right)
+            root->right->next = (root->next)?root->next->left:NULL;
+        
+        connect(root->left);
+        connect(root->right);
+    }
+    Node* connect(Node* root) {
+        connect_util(root);
+        return root;
+        
+    }
+};
