52785b8a13640f9bf82044c6a0c9de9f6b5be0a2
KHUSHAL GUMGAONKAR <khushaluniversal@users.noreply.github.com>
2022-01-21 20:21:00 +0530

    Create Student_Attendance_Record_I.cpp

---
+++ Student_Attendance_Record_I.cpp
@@ -0,0 +1,69 @@
+/*
+You are given a string s representing an attendance record for a student where each character signifies whether the student was absent, late, or present on that day. The record only contains the following three characters:
+
+'A': Absent.
+'L': Late.
+'P': Present.
+The student is eligible for an attendance award if they meet both of the following criteria:
+
+The student was absent ('A') for strictly fewer than 2 days total.
+The student was never late ('L') for 3 or more consecutive days.
+Return true if the student is eligible for an attendance award, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "PPALLP"
+Output: true
+Explanation: The student has fewer than 2 absences and was never late 3 or more consecutive days.
+Example 2:
+
+Input: s = "PPALLL"
+Output: false
+Explanation: The student was late 3 consecutive days in the last 3 days, so is not eligible for the award.
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s[i] is either 'A', 'L', or 'P'.
+*/
+
+class Solution {
+public:
+    bool checkRecord(string s) {
+        
+        int len = s.length();
+        int acount = 0;
+        int lcount = 1;
+        
+        if(len <= 1)
+            return true;
+        
+        //checking late and absent count
+        for(int i = 0; i < len; ++i)
+        {
+            if((i > 0) && (s[i-1] == 'L') && (s[i] == s[i-1]))
+                lcount++;
+            
+            //If student is not late for 3 consecutive day then reset count to 1
+            else if(lcount < 3)
+                    lcount = 1;
+            
+            
+            if(s[i] == 'A')
+                acount++;
+            
+        }
+        //cout<<acount<<" "<<lcount<<endl;
+        if(lcount >= 3 )
+            return false;
+        
+        if(acount >= 2)
+            return false;
+        
+        return true;
+        
+    }
+};
